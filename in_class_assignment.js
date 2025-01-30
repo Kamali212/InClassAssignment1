17ce8ea1511eca0062754bfd2252eea3c2b987f6
Kamali <Kamalikh3@gmail.com>
2025-01-30 17:36:50 -0500

    in class assignment

---
+++ in_class_assignment.js
@@ -0,0 +1,40 @@
+let expenses = 6780; //Declare variable 
+
+if (expenses > 7000) { // Check if expenses are greater than 7000
+    console.log("High Expenses")
+} else { 
+    console.log("Manageable Expenses") // If less than 7000 print manageable expenses\   
+}
+
+// Task 2
+
+for (let i = 10; i >=1; i--) {
+    console.log(i);
+    
+}
+
+// Task 3
+
+let count = 2;
+while (count <=10) {
+    console.log("Count is:" + count);
+    count+= 2;
+}
+
+// Task 4
+
+function calculateSquare(number)
+{ 
+return number * number 
+}
+
+// Task 5
+
+const doublenumber = num => num * 2;
+console.log(doublenumber(3));
+
+// Task 6
+
+const numbers = [3, 6, 9];
+const doublednumber = numbers.map( num => num * num);
+console.log(doublesdnumbers)
